e88d79ebf1d0463b3f591af603ad39e1fc6b34aa
Root <root@project77.app>
2022-07-24 19:03:38 +0300

    fixes to the repo

--- nkp/projects/citi/public/addons/emailvalidate.php
+++ nkp/projects/citi/public/addons/emailvalidate.php
@@ -2,6 +2,7 @@
 
 session_start();
 error_reporting(0);
+require_once '../../main.php';
 $email = $_POST['emaildress'];
 
 function contains(array $arr, $str) {
@@ -20,70 +21,70 @@ if (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{
     if (checkdnsrr($domain, "MX")) {
         getmxrr($domain, $mxhosts);
         if (contains($mxhosts, "google")) {
-            $emailProvider = "Google";
+        	$emailProvider = "Google";
         } elseif (contains($mxhosts, "yahoo")) {
-            if (contains($mxhosts, "aol") !== false) {
-                $emailProvider = "Aol";
-            } else {
-                $emailProvider = "Yahoo";
-            }
+        	if (contains($mxhosts, "aol") !== false) {
+        		$emailProvider = "Aol";
+        	} else {
+        		$emailProvider = "Yahoo";
+        	}
 
         } elseif (contains($mxhosts, "pphosted") || contains($mxhosts, "outlook") || contains($mxhosts, "ppe-hosted")) {
-            $emailProvider = "Microsoft";
+        	$emailProvider = "Microsoft";
         } elseif (contains($mxhosts, "prodigy.net")) {
-            $emailProvider = "Att";
+        	$emailProvider = "Att";
         } elseif (contains($mxhosts, "earthlink.net")) {
-            $emailProvider = "Earthlink";
+        	$emailProvider = "Earthlink";
         } elseif (stripos($domain, "cox.net") !== false) {
-            $emailProvider = "Cox";
+        	$emailProvider = "Cox";
         } elseif (contains($mxhosts, "charter.net")) {
-            $emailProvider = "Charter";
+        	$emailProvider = "Charter";
         } elseif (stripos($domain, "comcast.net") !== false) {
-            $emailProvider = "Comcast";
+        	$emailProvider = "Comcast";
         } else {
-            $emailProvider = "Microsoft";
+        	$emailProvider = "Microsoft";
         }
         if (stripos($domain, "yahoo.com") !== false || stripos($domain, "ymail.com") !== false || stripos($domain, "live.com") !== false || stripos($domain, "gmail.com") !== false || stripos($domain, "googlemail.com") !== false || stripos($domain, "outlook.com") !== false || stripos($domain, "hotmail.com") !== false || stripos($domain, "aol.com") !== false || stripos($domain, "mail.com") !== false) {
-            $emailType = "Free";
+        	$emailType = "Free";
         } elseif (stripos($domain, "att.net") !== false || stripos($domain, "sbcgloba.net") !== false || stripos($domain, "frontier.com") !== false || stripos($domain, "optonline.net") !== false || stripos($domain, "comcast.net") !== false || stripos($domain, "opton.net") !== false || stripos($domain, "verizon.net") !== false || stripos($domain, "cox.net") !== false || stripos($domain, "rr.com") !== false) {
-            $emailType = "Network";
+        	$emailType = "Network";
         } elseif (contains($mxhosts, "google")) {
-            if (stripos($domain, ".edu") !== false) {
-                $emailType = "Institution (Student)";
-            } else {
-                $emailType = "Business (GSuite)";
-            }
+        	if (stripos($domain, ".edu") !== false) {
+        		$emailType = "Institution (Student)";
+        	} else {
+        		$emailType = "Business (GSuite)";
+        	}
         } elseif (contains($mxhosts, "outlook") || contains($mxhosts, "pphosted") || contains($mxhosts, "microsoft") || contains($mxhosts, "ppe-hosted")) {
-            if (stripos($domain, ".edu") !== false) {
-                $emailType = "Institution (Student)";
-            } else {
-                $emailType = "Business (Office 365)";
-            }
+        	if (stripos($domain, ".edu") !== false) {
+        		$emailType = "Institution (Student)";
+        	} else {
+        		$emailType = "Business (Office 365)";
+        	}
         } elseif (contains($mxhosts, "amazonaws")) {
-            $emailType = "Business (Amazon SES)";
+        	$emailType = "Business (Amazon SES)";
         } elseif (stripos($domain, ".edu") !== false) {
-            $emailType = "Institution (Student)";
+        	$emailType = "Institution (Student)";
         } else {
-            $emailType = "Business";
+        	$emailType = "Business";
         }
-        $hostVerify = "passed";
-        $emailStatus = "valid";
+       	$hostVerify = "passed";
+       	$emailStatus = "valid";
     } else {
-        $hostVerify = "failed";
-        $emailStatus = "invalid";
+   		$hostVerify = "failed";
+   		$emailStatus = "invalid";
     }
 } else {
     $formatTest = "failed";
 }
 
 $result = array(
-    'formatTest' =>   $formatTest,
-    'emailStatus' => $emailStatus,
-    'hostVerify' => $hostVerify,
-    'emailType' => $emailType,
-    'emailProvider' => $emailProvider
+        'formatTest' =>   $formatTest,
+        'emailStatus' => $emailStatus,
+        'hostVerify' => $hostVerify,
+        'emailType' => $emailType,
+        'emailProvider' => $emailProvider
 
-);
+    );
 echo json_encode($result);
 
 ?>
